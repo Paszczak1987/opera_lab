699feb4fcf372068e20cdcd9aa017a17036ba5b9
Lukasz Paszko <PaszkoLuk@sbsit.com>
2025-10-07 22:13:51 +0200

    fix urls.py warning

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     # path('users/', include('users.urls')),
-    path('__reload__/', include('django_browser_reload.urls')),
+    # path('__reload__/', include('django_browser_reload.urls')),
 
 ]
 
