260d5f1ad79a6d4fe3d15ecf9e20ad3d8128ef24
Lukasz Paszko <PaszkoLuk@sbsit.com>
2025-10-07 21:31:18 +0200

    user models & admin panel registration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('client', 'Client'),
+        ('manager', 'Manager'),
+        ('technician', 'Technician'),
+        ('viewer', 'Viewer'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    # ewentualnie: przypisanie do laboratorium dodamy później w modelu Laboratory
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
