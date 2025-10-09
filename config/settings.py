fb288fa2d8bcf4ce42f79a32c24a030dac7ed84b
Lukasz Paszko <PaszkoLuk@sbsit.com>
2025-10-09 20:49:11 +0200

    improved layout nav working login also

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.User'
 
-LOGIN_URL = '/'
+LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/welcome/'
 LOGOUT_REDIRECT_URL = '/'
 
--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.HomeMainView.as_view(), name='home'),
-    path('welcome/', views.WelcomeView.as_view(), name='welcome'),
+    path('logged_in/', views.WelcomeView.as_view(), name='logged_in'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
