2a573395a7f9a7655775dd9cf2de9f7b79e72629
Lukasz Paszko <PaszkoLuk@sbsit.com>
2025-11-10 11:57:27 +0100

    added contry code to user

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,17 @@ from .models import User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
-        ("Role i laboratoria", {"fields": ("role", "active_laboratory")}),
+        ("Role i laboratoria", {"fields": ("role", "active_laboratory", "country_code", "country_name")}),
     )
-    list_display = ("username", "email", "first_name", "last_name", "role", "active_laboratory", "is_active")
-    list_filter = ("role", "is_active")
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "country_code",
+        "active_laboratory",
+        "is_active",
+    )
+    list_filter = ("role", "is_active", "country_code")
+    readonly_fields = ("country_name",)
